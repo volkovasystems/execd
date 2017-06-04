f8dbf924554c5984865391ae39785731ee609b6e
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-04 15:58:19 +0800

    Updated build.

--- execd.support.js
+++ execd.support.js
@@ -35,6 +35,9 @@
               			"module": "execd",
               			"author": "Richeve S. Bebedor",
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+              			],
               			"repository": "https://github.com/volkovasystems/execd.git",
               			"test": "execd-test.js",
               			"global": true
