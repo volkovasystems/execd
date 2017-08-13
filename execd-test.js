9814f480af3d4a51de6687966186c345687b0180
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-13 13:36:48 +0800

    Updated test.

--- execd-test.js
+++ execd-test.js
@@ -3,9 +3,9 @@ const assert = require( "assert" );
 const execd = require( "./execd.js" );
 
 let hello = function hello( ){ };
-assert.equal( execd( hello ), false, "should be false" );
+assert.equal( execd( hello ), false, "should be equal to false" );
 
 hello[ Symbol.for( "called" ) ] = Symbol.for( "called" );
-assert.equal( execd( hello ), true, "should be true" );
+assert.equal( execd( hello ), true, "should be equal to true" );
 
 console.log( "ok" );
